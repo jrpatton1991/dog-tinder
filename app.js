da11fafc322338f34c62a7646428ef2ae1a57063
Haydeé Muñoz <haydeemunozdelarocha@gmail.com>
2016-04-26 16:30:35 -0500

    hayde changes

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-mongoose.connect(process.env.DB_CONN_DOG_TINDER);
+mongoose.connect(process.env.DB_CONN_GA_WDI5ATX);
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
